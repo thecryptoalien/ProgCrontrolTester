32c27a395fa3103c856bc7f49372214ec5461807
thecryptoalien@gmail.com <thecryptoalien@gmail.com>
2022-01-21 23:07:26 -0500

    a few cleanup things

--- ProgCrontrolTester/Program.cs
+++ ProgCrontrolTester/Program.cs
@@ -17,30 +17,37 @@ namespace ProgCrontrolTester
         static void Main(string[] args)
         {
             Console.WriteLine("Starting tester....");
+            // using maincrontrol
             using (MainCtrl mainCtrl = new MainCtrl())
             {
+                // using process from maincrontrol
                 using (Process ctrlPipe = mainCtrl.RunProg(@"C:\Windows\System32\cmd.exe"))
                 {
+                    // start process
                     ctrlPipe.Start();
                     progRunnign = true;
                     StreamReader streamReader = ctrlPipe.StandardOutput;
                     StreamWriter myStreamWriter = ctrlPipe.StandardInput;
                     String inputText = " ";
+
+                    // init and start tasks
                     Task outtask = new Task(dOutput);
                     outtask.Start();
                     Task intask = new Task(dInput);
                     intask.Start();
+
+                    // internal output task
                     void dOutput()
                     {
+                        // loop until forever
                         while (true)
                         {
-                            //string line = streamReader.ReadLine();
-                            //Console.WriteLine(line);
+                            // init buffer
                             char[] charArray = new char[10000];
                             var readByteCount = streamReader.Read(charArray, 0, charArray.Length);
                             if (readByteCount > 0)
                             {
-                                //string line = streamReader.ReadLine();
+                                // form string from buffer
                                 String data = new string(charArray, 0, readByteCount);
                                 string[] datas = data.Split(new string[] { "\r\n" }, StringSplitOptions.None);
                                 for (int i = 0; i < datas.Length; i++)
@@ -56,31 +63,33 @@ namespace ProgCrontrolTester
                                             Console.Write(datas[i].Replace("\r", "").Replace("\n", "") + "\r\n");
                                         }
                                     }
-                                    //Thread.Sleep(15);
                                 }
-
                             }
+                            // nullify buffer
                             charArray = null;
-                            //Thread.Sleep(5);
-
                         }
                     }
+
+                    // internal input task
                     void dInput()
                     {
-                        do
+                        // loop untill quit
+                        while (progRunnign)
                         {
+                            // read in text
                             inputText = Console.ReadLine();
-                            
+                            // write to stream
                             myStreamWriter.WriteLine(inputText);
                             myStreamWriter.Flush();
-                            
+                            // check for exit
                             if (inputText == "exit")
                             {
                                 progRunnign = false;
                             }
-
-                        } while (progRunnign);
+                        } 
                     }
+
+                    // await standard input
                     intask.Wait();
                 }
             }
